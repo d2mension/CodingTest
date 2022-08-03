15284c38c1072f67c4485d6e1735de56834786ce
d2mension <d.2mension78@gmail.com>
2022-08-03 22:25:20 +0900

    2022 summer

---
+++ programmers/3진법 뒤집기.py
@@ -0,0 +1,24 @@
+def solution(n):
+    answer = 0
+    trit = ''
+    num = 0
+
+    while n > 0:
+        trit += str(n % 3)
+        n //= 3
+
+    length = len(trit)-1
+    trit = int(trit)
+    
+    for i in range(length, -1, -1):
+        digit_10 = 10 ** i
+        num = trit // digit_10
+        trit %= digit_10
+        
+        answer += (num * (3 ** i))
+        print(i, digit_10, num, trit)
+
+    print(answer)
+        
+
+solution(45)
